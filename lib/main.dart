9086068ecbccfcc2347579fc3797c720a367292e
Felipe de Paula <emilcyfelipe@gmail.com>
2021-06-22 15:18:35 -0300

    home/login/splash-pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:pay_flow_nlw/modules/login_page.dart';
+import 'package:pay_flow_nlw/modules/splash_page.dart';
+import 'package:pay_flow_nlw/shared/themes/app_colors.dart';
 
+void main(){
+  runApp(AppWidget());
+}
+
+class AppWidget extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+    title: 'Pay Flow',
+    theme: ThemeData(
+      primaryColor: AppColors.primary),
+      home: LoginPage(),
+
+  );
+  }
+}
